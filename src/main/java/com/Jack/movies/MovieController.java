5bffbaebd732c58d26ffc464b4408f0116b4946f
JOBCOMETOME <94985218+JOBCOMETOME@users.noreply.github.com>
2023-12-16 23:23:28 -0500

    This is MovieApp backend.

---
+++ src/main/java/com/Jack/movies/MovieController.java
@@ -0,0 +1,28 @@
+package com.Jack.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+@CrossOrigin(origins = "*")
+public class MovieController {
+
+    @Autowired
+    private MoviesService moviesService;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        return new ResponseEntity<List<Movie>>(moviesService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId) {
+        return new ResponseEntity<>(moviesService.singleMovie(imdbId), HttpStatus.OK);
+    }
+}
---
+++ src/test/java/com/Jack/movies/MovieApplicationTests.java
@@ -0,0 +1,15 @@
+package com.Jack.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootTest
+
+class MovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
